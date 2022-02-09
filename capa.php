22970563b32b933a7a7d2cf561ae2a238b3bf887
Davi Marques Gonçalves <davisigon@gmail.com>
2022-02-08 22:24:09 -0300

    first commit

---
+++ capa.php
@@ -0,0 +1,46 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurante Bolevart</title>
+    <link rel="stylesheet" href="capa.css">
+    <style>
+        .button {
+            text-align: center;
+            text-transform: uppercase;
+            font-weight: bold;
+            font-family: Helvetica;
+            border: none;
+            height: 45px;
+            border-radius: 10px;
+            margin-top: 30px;
+            color: #FFF;
+            background-color: #ff477e;
+            cursor: pointer;
+
+        }
+
+        .button:hover {
+            background-color: #afb1ff;
+            transition: .5s;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="telefone-container">
+        <h1>Seja Bem-Vindo!!!!</h1>
+        <p>Te aguardamos ansiosos!</p>
+        <p>Se ainda não nos conhece se cadastre e acesse o nosso Restaurante!</p>
+        <a href="login.php">
+            <button class="button"> Entrar</button>
+        </a>
+    </div>
+</body>
+
+</html>
---
+++ final.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Final</title>
+    <link rel="stylesheet" href="final.css">
+</head>
+
+<body>
+    <div id="telefone-container">
+        <h1>Seu Pedido foi concluido com sucesso!</h1>
+        <p>Nosso estabelecimento agradece pela a preferência!!!</p>
+        <p>Volte Sempre!!</p>
+        <div id="telefone-container2">
+            <a href="capa.php"> Clique aqui e volte a tela inicial</a>
+        </div>
+    </div>
+</body>
+
+</html>
